591369faf62f14217963c5bd21ca8a3ce43e82ae
ymmmtym <h.yukihisa.378@gmail.com>
2020-01-19 18:41:31 +0900

    add table into index

--- scripts/create_index.py
+++ scripts/create_index.py
@@ -1,7 +1,13 @@
+# -*- coding: utf-8 -*-
+
 import os
 import glob
 import re
+import sys
+import io
+from bs4 import BeautifulSoup
 from jinja2 import Template, Environment, FileSystemLoader
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 
 env = Environment(loader=FileSystemLoader('./templates'))
@@ -9,14 +15,17 @@ template = env.get_template('index.html.j2')
 
 path = './tracklists/'
 items = []
+trans_dict = {'<h1>': '<h3>', '</h1>': '</h3>'}
 
 for i in os.listdir(path):
   item = {}
   item['name'] = re.findall('mix-(.+).html', i)[0]
   item['url'] = path + i
   item['genre'] = re.findall('mix-(\D+)\d+.html', i)[0]
+  item['table'] = BeautifulSoup(open(item['url']), "lxml").find('table')
   items.append(item)
 
+items = sorted(items, key=lambda x:x['name'])
 genres = sorted(set([i['genre'] for i in items]))
 data = {}
 
@@ -27,8 +36,7 @@ for genre in genres:
       data[genre].append(i)
 
 render_data = {
-  "data": data,
-  "genres": genres
+  "data": data
 }
 
 rendered = template.render(render_data)
